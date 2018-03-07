a9ea5b7e6f98d8e6533beea841e8d19b0af4ac32
james <james.taggart.28@gmail.com>
2018-03-07 16:35:13 -0600

    created / included apps app

--- bug-track/config/urls.py
+++ bug-track/config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('projects/', include('projects.urls')),
+    path('apps/', include('apps.urls')),
 ]
