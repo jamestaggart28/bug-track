bd0fe8fa17b53bd30cefee7edd446e6e4ae98463
james <james.taggart.28@gmail.com>
2018-03-08 09:54:57 -0600

    created project views/urls/templates

--- bug-track/projects/urls.py
+++ bug-track/projects/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from .views import ProjectCreate, ProjectUpdate, ProjectDelete, ProjectList, ProjectDetail
 
 app_name = "projects"
 
 urlpatterns = [
-
+    path('', ProjectCreate.as_view(), name="project-create"),
+    path('<int:pk>/', ProjectDetail.as_view(), name="project-detail"),
+    path('<int:pk>/update/', ProjectUpdate.as_view(), name="project-update"),
+    path('<int:pk>/delete/', ProjectDelete.as_view(), name="project-delete"),
+    path('list/', ProjectList.as_view(), name="project-list"),
 ]
--- bug-track/projects/views.py
+++ bug-track/projects/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Project, Bug
+
 
 # Create your views here.
+class ProjectCreate(CreateView):
+    model = Project
+    fields = ['name', 'description']
+
+
+class ProjectUpdate(UpdateView):
+    model = Project
+    fields = ['name', 'description']
+
+
+class ProjectDelete(DeleteView):
+    model = Project
+    success_url = reverse_lazy('project:project-list')
+
+
+class ProjectDetail(DetailView):
+    model = Project
+
+
+class ProjectList(ListView):
+    model = Project
