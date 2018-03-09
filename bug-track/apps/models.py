e736c2b414b10cd6fcadfe2fc0f37c743699a684
james <james.taggart.28@gmail.com>
2018-03-09 12:04:36 -0600

    created application/bug models

--- bug-track/apps/models.py
+++ bug-track/apps/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from projects.models import Project
+
 
 # Create your models here.
+class Application(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    projects = models.ManyToManyField(Project)
+
+    def __str__(self):
+        return self.name
+
+
+class Bug(models.Model):
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    reproduce = models.TextField()
+    expected = models.TextField()
+    observed = models.TextField()
+    assigned = models.CharField(max_length=30)
+    fiexed = models.BooleanField()
+
+    def __str__(self):
+        return self.name
