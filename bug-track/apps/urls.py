8b29f97f2d08934dcb131270adb4850ae7c24b1a
james <james.taggart.28@gmail.com>
2018-03-10 18:52:39 -0600

    created app bug urls

--- bug-track/apps/urls.py
+++ bug-track/apps/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+from django.urls import include, path
 from .views import AppCreate, AppUpdate, AppDelete, AppList, AppDetail
+from .views import BugCreate, BugUpdate, BugDelete, BugList, BugDetail
 
 app_name = "apps"
+
+
+bug_patterns = [
+    path('', BugCreate.as_view(), name="bug-create"),
+    path('<int:pk>/', BugDetail.as_view(), name="bug-detail"),
+    path('<int:pk>/update/', BugUpdate.as_view(), name="bug-update"),
+    path('<int:pk>/delete/', BugDelete.as_view(), name="bug-delete"),
+    path('list/', BugList.as_view(), name="bug-list"),
+]
 
 
 urlpatterns = [
@@ -10,4 +20,5 @@ urlpatterns = [
     path('<int:pk>/update/', AppUpdate.as_view(), name="app-update"),
     path('<int:pk>/delete/', AppDelete.as_view(), name="app-delete"),
     path('list/', AppList.as_view(), name="app-list"),
+    path('bugs/', include(bug_patterns)),
 ]
