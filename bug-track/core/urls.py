eb77f2a115195a5fcf24f0bd07fc144680e15b73
james <james.taggart.28@gmail.com>
2018-03-15 14:38:03 -0500

    created user registration

--- bug-track/core/urls.py
+++ bug-track/core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from core.views import Home
+from core.views import Home, Register
+from django.conf.urls import include
 
 app_name = "core"
 
+account_patterns = [
+    path('registration/', Register.as_view(), name="register")
+]
+
 urlpatterns = [
-    path('', Home.as_view(), name="home")
+    path('accounts/', include(account_patterns)),
+    path('', Home.as_view(), name="home"),
 ]
--- bug-track/core/views.py
+++ bug-track/core/views.py
@@ -1,6 +1,23 @@
 from django.views.generic import TemplateView
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import FormView
+from django.shortcuts import redirect
 
 
 # Create your views here.
 class Home(TemplateView):
     template_name = "core/home.html"
+
+
+class Register(FormView):
+    form_class = UserCreationForm
+    template_name = 'registration/register.html'
+
+    def form_valid(self, form):
+        form.save()
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
+        login(self.request, user)
+        return redirect('core:home')
